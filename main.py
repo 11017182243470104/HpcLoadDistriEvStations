d4fdbd3d88c24d33802d1fe30aeb57296baeff67
11017182243470104 <42222345+11017182243470104@users.noreply.github.com>
2019-05-03 12:22:28 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # main file with procedure
-
 from random_generator import *
 from opt_model_pwl import *
 import openpyxl
@@ -13,14 +12,22 @@ wb = openpyxl.Workbook()
 ws1 = wb.active
 ws1.title = "Multi-CPO-Setting"
 ws1["A1"] = "Run"
-ws1["B1"] = "Opt variance"
+ws1["B1"] = "Opt objective value "
 ws1["C1"] = "Opt average"
 ws1["D1"] = "Opt max"
 ws1["E1"] = "Opt min"
-ws1["F1"] = "Smpl variance"
+ws1["F1"] = "Smpl objective value"
 ws1["G1"] = "Smpl average"
 ws1["H1"] = "Smpl max"
 ws1["I1"] = "Smpl min"
+ws1["J1"] = "Opt objective val w real exp"
+ws1["K1"] = "Opt average w real exp"
+ws1["L1"] = "Opt max w real exp"
+ws1["M1"] = "Opt min w real exp"
+ws1["N1"] = "Smpl objective val w real exp"
+ws1["O1"] = "Smpl average w real exp"
+ws1["P1"] = "Smpl max w real exp"
+ws1["Q1"] = "Smpl min w real exp"
 
 # Filling the Excel file with data
 count = 0
@@ -30,7 +37,7 @@ for run in range(int(samplesize)):
     print("simple: ", solution[1])
 
     ws1.cell(column=1, row=run + 2, value=run + 1)
-    for k in range(2):
+    for k in range(4):
         for i in range(4):
             ws1.cell(column=2 + i + (k * 4), row=run + 2, value=solution[k][i])
 
